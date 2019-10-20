c8d1a32163061a487a2093a6126d2ed3893550c9
havanur <50772127+havanur@users.noreply.github.com>
2019-10-20 16:43:44 +0300

    Update sayiTabanUygulamasi.java

--- SayiTabanUygulamasi/src/SayiTabanUygulamasi/sayiTabanUygulamasi.java
+++ SayiTabanUygulamasi/src/SayiTabanUygulamasi/sayiTabanUygulamasi.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package SayiTabanUygulamasi;
 import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 
-/**
- *
- * @author HAYRİ
- */
 public class sayiTabanUygulamasi extends javax.swing.JFrame {
   
     JTextField field = new JTextField();
